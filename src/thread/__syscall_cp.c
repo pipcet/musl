6289e06a784233df49347365328da69e3d93e3d8
Pip Cet <pipcet@gmail.com>
2016-12-06 22:45:59 +0000

    workaround

--- src/thread/__syscall_cp.c
+++ src/thread/__syscall_cp.c
@@ -8,7 +8,7 @@ static long sccp(syscall_arg_t nr,
                  syscall_arg_t u, syscall_arg_t v, syscall_arg_t w,
                  syscall_arg_t x, syscall_arg_t y, syscall_arg_t z)
 {
-	return (__syscall)(nr, u, v, w, x, y, z);
+	return (__syscall6)(nr, u, v, w, x, y, z);
 }
 
 weak_alias(sccp, __syscall_cp_c);
@@ -17,5 +17,5 @@ long (__syscall_cp)(syscall_arg_t nr,
                     syscall_arg_t u, syscall_arg_t v, syscall_arg_t w,
                     syscall_arg_t x, syscall_arg_t y, syscall_arg_t z)
 {
-	return __syscall_cp_c(nr, u, v, w, x, y, z);
+	return sccp(nr, u, v, w, x, y, z);
 }
