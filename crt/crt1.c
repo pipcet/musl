39d01deb81c37141d3c78d34793a093dbe2af601
Pip Cet <pipcet@gmail.com>
2016-12-09 22:37:02 +0000

    define an ordinary _start function

--- crt/crt1.c
+++ crt/crt1.c
@@ -5,14 +5,12 @@
 #include "crt_arch.h"
 
 int main();
-void _init() __attribute__((weak));
-void _fini() __attribute__((weak));
 _Noreturn int __libc_start_main(int (*)(), int, char **,
 	void (*)(), void(*)(), void(*)());
 
-void _start_c(long *p)
+void _start(int, char **, char **) __attribute__((stackcall));
+
+void _start(int argc, char **argv, char **envp)
 {
-	int argc = p[0];
-	char **argv = (void *)(p+1);
-	__libc_start_main(main, argc, argv, _init, _fini, 0);
+        __libc_start_main(main, argc, argv, 0, 0, 0);
 }
